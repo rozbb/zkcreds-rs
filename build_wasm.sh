1fa8f85d008ae622b94272bc5830e0b9c64485e0
Michael Rosenberg <michael@mrosenberg.pub>
2022-02-14 12:53:16 -0500

    Removed WASM stuff

--- build_wasm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wasm-pack build --release --target web -- --no-default-features --features "std"
--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,4 @@ mod proof_of_issuance;
 mod sparse_merkle;
 mod test_util;
 
-#[cfg(not(feature = "no-wasm"))]
-pub mod wasm_api;
-
 pub type Error = Box<dyn ark_std::error::Error>;
